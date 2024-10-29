ccf500bff09bc3bc4485537df69ea5d5c4b018ef
ankitkr-04 <ak0182274@gmail.com>
2024-10-29 07:06:14 +0530

    add sanity

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,5 @@
 import SearchForm from "@/components/SearchForm";
+import StartupCard from "@/components/StartupCard";
 
 export default async function Home({
   searchParams,
@@ -8,16 +9,44 @@ export default async function Home({
   }>;
 }) {
   const query = (await searchParams).query || "";
+  const posts = [
+    {
+      _createdAt: new Date(),
+      views: 100,
+      author: { _id: "1", name: "John Doe" },
+      _id: "1",
+      description: "A platform for connecting with other entrepreneurs",
+      image:
+        "https://images.unsplash.com/photo-1667986292516-f27450ae75a9?q=80&w=1976&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      category: "Robots",
+      title: "Robotics Startup",
+    },
+  ];
+
   return (
-    <section className="pink_container">
-      <h1 className="heading">
-        Pitch Your Startup,
-        <br /> Connect With Enterpreneurs
-      </h1>
-      <p className="sub-heading !max-w-3xl">
-        Submit Ideas, Vote on Pitches, and Get Noticed in Virtual Competitions
-      </p>
-      <SearchForm query={query} />
-    </section>
+    <>
+      <section className="pink_container">
+        <h1 className="heading">
+          Pitch Your Startup,
+          <br /> Connect With Enterpreneurs
+        </h1>
+        <p className="sub-heading !max-w-3xl">
+          Submit Ideas, Vote on Pitches, and Get Noticed in Virtual Competitions
+        </p>
+        <SearchForm query={query} />
+      </section>
+      <section className="section_container">
+        <p className="text-30-semibold">
+          {query ? `Search results for "${query}"` : "Latest Pitches"}
+        </p>
+        <ul className="mt-7 card_grid">
+          {posts?.length > 0 ? (
+            posts.map((post) => <StartupCard key={post?._id} post={post} />)
+          ) : (
+            <p className="no-results">No Startups Found</p>
+          )}
+        </ul>
+      </section>
+    </>
   );
 }
